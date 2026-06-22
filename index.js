3985571f4e2c9f89b2dbc62d811acfaa9c1b2af2
xd8436408-prog <xd8436408@gmail.com>
2026-06-22 21:13:13 +0300

    Update index.js

--- index.js
+++ index.js
@@ -16,9 +16,9 @@ app.listen(PORT, () => {
 const token = process.env.TOKEN;
 const message = process.env.MESSAGE;
 const channels = [
-  "1515700897630650378",
-  "1515700897630650378",
-  "1515700897630650378"
+  "1518675449893355570",
+  "1518675449893355570",
+  "1518675449893355570"
 ];
 
 let currentIndex = 0;
